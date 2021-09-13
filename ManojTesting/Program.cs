4f6e91883910101d01d66754db3072f6948a3e3b
Kumar, Chhotan <Chhotan_Kumar@Dell.com>
2021-09-13 11:15:19 +0530

    Add project files.

---
+++ ManojTesting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ManojTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Manoj");
+        }
+    }
+}
