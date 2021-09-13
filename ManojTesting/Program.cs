b07e10fd4b6683d81d192cddd5925741de1d4986
Kumar, Chhotan <Chhotan_Kumar@Dell.com>
2021-09-13 11:22:17 +0530

    Update in Code

--- ManojTesting/Program.cs
+++ ManojTesting/Program.cs
@@ -8,6 +8,7 @@ namespace ManojTesting
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Manoj");
+            Console.WriteLine("Manoj : The Star of GII when Anil is not around");
         }
     }
 }
